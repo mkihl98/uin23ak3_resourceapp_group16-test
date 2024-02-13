a52f6017beb826e97f0f20bbd1e0cbb7c84b0c61
CakeDsx <111729620+CakeDsx@users.noreply.github.com>
2024-02-13 14:41:53 +0100

    accepted

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,7 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
   
-=======
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      
->>>>>>> 6f5312fa9bb4d6046c8b904d9690cf8f602fb26d
     </>
   )
 }
