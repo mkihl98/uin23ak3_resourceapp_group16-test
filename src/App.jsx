84204240437864b8c23d9a2c30e30e2ccfd88ff7
mkihl98 <martinki@hiof.no>
2024-03-05 15:35:04 +0100

    Prøvd å lage en funksjon

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
+import {Route, Routes} from 'react-router-dom'
 import './App.css'
+import Html from './components/Html'
 import Layout from './components/Layout'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
   const [content, setContent] = useState([])
 
+  const getData = async()=>{
+    try{
+      const response = await fetch(content)
+      const data = await response.json()
+      setContent(data)
+    }catch{
+      console.error("Det har skjedd en feil")
+    }
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
   return (
-    <>
-      <p>Hei</p>
-      <p>Hei</p>
-    </>
+    <Layout>
+      <Routes>
+        <Route index element={<Html content={content}/>}/>
+        <Route element={<CSS />}/>
+        <Route element={<JavaScript />}/>
+        <Route element={<React />}/>
+        <Route element={<Sanity />}/>
+      </Routes>
+    </Layout>
   )
 }
 
