3f9eb58f67b500571648329615562c5352d219fc
mkihl98 <martinki@hiof.no>
2024-03-05 14:28:56 +0100

    Startet opp routing
    Lagt til routing i App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-
-
-      <p>hei</p>
-      <p>Hei</p>
-      <p>Hei</p>
-
-    </>
+    <Layout>
+      <Routes>
+        <Route index element={<HTML />}/>
+        <Route element={<CSS />}/>
+        <Route element={<JavaScript />}/>
+        <Route element={<React />}/>
+        <Route element={<Sanity />}/>
+      </Routes>
+    </Layout>
   )
 }
 
