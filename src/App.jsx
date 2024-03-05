abe4699804b25016631eac39cd37add403fe3344
mkihl98 <martinki@hiof.no>
2024-03-05 15:20:49 +0100

    Importert useState til App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Layout from './components/Layout'
+import { useState } from 'react'
 
 function App() {
   const [content, setContent] = useState([])
